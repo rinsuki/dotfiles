1b0ba0c02175f9d643f94145c63d94b1670bbdcc
rinsuki <6533808+rinsuki@users.noreply.github.com>
2017-12-28 06:26:26 +0900

    fix

--- install.py
+++ install.py
@@ -5,4 +5,6 @@ import sys
 NOW_DIR = os.path.dirname(os.path.realpath(__file__))
 
 for file in os.listdir(NOW_DIR + "/home/"):
+    if os.path.exists(os.environ["HOME"] + "/" + file):
+        os.remove(os.environ["HOME"] + "/" + file)
     os.symlink(NOW_DIR + "/home/" + file, os.environ["HOME"] + "/" + file)
