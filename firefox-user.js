9f388dd6319b4fa47a91796e3ea9e10c3a93159c
rinsuki <428rinsuki+git@gmail.com>
2020-06-30 11:49:30 +0900

    add firefox-user.js

---
+++ firefox-user.js
@@ -0,0 +1,5 @@
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("security.secure_connection_icon_color_gray", false);
+user_pref("font.name.monospace.ja", "Menlo");
