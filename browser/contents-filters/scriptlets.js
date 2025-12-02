83047a55d72666ca545ab433844368594e4e572e
rinsuki <428rinsuki+git@gmail.com>
2025-12-02 15:45:32 +0900

    try scriptlets

---
+++ browser/contents-filters/scriptlets.js
@@ -0,0 +1,8 @@
+/// <reference lib="dom" />
+
+/// typekit-replace.js
+(function() {
+    document.documentElement.classList.add("wf-active")
+    document.documentElement.classList.remove("wf-inactive")
+    document.documentElement.classList.remove("wf-loading")
+})()
